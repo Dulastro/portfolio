3d7f34e33df3984c55fe5d0b4e800f4b4ea97299
Pedro Dulastro <pedro.dulastro.silva@gmail.com>
2024-07-26 08:49:01 -0300

    Updates

--- src/components/About/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.css";
-export default function About({ about, aboutDescription, id }) {
-  return (
-    <section className={styles.about} id={id}>
-      <h2>{about}</h2>
-      <p>{aboutDescription}</p>
-    </section>
-  );
-}
